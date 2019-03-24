c3d1c9d3e3020e63a55f0b911ae374361eea9777
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-25 02:48:07 +0900

    Multi interface inheritance

---
+++ practise1/practise/interface/Interface03.cs
@@ -0,0 +1,40 @@
+﻿// multi interface inheritance
+using System;
+
+namespace practise1
+{
+    interface IRunnable
+    {
+        void Run();
+    }
+    interface IFlyable
+    {
+        void Fly();
+    }
+    class FlyingCar : IRunnable, IFlyable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Run! Run!");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Fly! Fly!");
+        }
+    }
+    class Interface03
+    {
+        static void Main(string[] args)
+        {
+            FlyingCar car = new FlyingCar();
+            car.Run();
+            car.Fly();
+
+            IRunnable runnable = car as IRunnable;
+            runnable.Run();
+
+            IFlyable flyable = car as IFlyable;
+            flyable.Fly();
+        }
+    }
+}
