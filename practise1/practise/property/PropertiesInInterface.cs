7203f85860eee1a9164f713afe3d802976051fcc
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-29 01:17:12 +0900

    TODO: (properties in interface)

---
+++ practise1/practise/property/PropertiesInInterface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//TODO:
+namespace practise1
+{
+    interface INameValue
+    {
+        string Name
+        {
+            get;
+            set;
+            }
+    }
+    string Value
+    {
+        get;
+        set;
+    }
+    c
+    class PropertiesInInterface
+    {
+
+        static void Main(string [] args)
+        {
+
+        }
+    }
+}
