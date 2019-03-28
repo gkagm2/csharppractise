8025baaf39142db3fb1235e17bb4e1b7cf9de394
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-29 00:36:16 +0900

    auto implemented property practise.

---
+++ practise1/practise/property/AutoImplementedProperty01.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1
+{
+    class BirthdayInfo
+    {
+        //auto implemented property
+        public string Name
+        {
+            get;
+            set;
+        }
+        //auto implemented property
+        public DateTime Birthday
+        {
+            get;
+            set;
+        }
+        public int Age
+        {
+            get
+            {
+            return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+    class AutoImplementedProperty01
+    {
+        static void Main(string [] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            birth.Name = "HyeonMyeong";
+            birth.Birthday = new DateTime(1993, 12, 30);
+
+            Console.WriteLine("Name : {0}", birth.Name);
+            Console.WriteLine("Birthday : {0}", birth.Birthday.ToShortDateString());
+            Console.WriteLine("Age : {0}", birth.Age);
+        }
+    }
+}
