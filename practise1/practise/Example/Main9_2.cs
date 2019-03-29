dd9717223cc28017f49d4f0ef7bd5fcc3a8c3d5f
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-30 05:13:08 +0900

    make code with anonymous type

---
+++ practise1/practise/Example/Main9_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//make Anonymous type
+namespace practise1
+{
+    class Main9_2
+    {
+        static void Main(string[] args)
+        {
+            //(Anonymous Type)무명 형식을 이용해서 완성
+            var nameCard = new { Name = "홍길동", Age = 20 };
+            Console.WriteLine("name : {0}, age : {1}", nameCard.Name, nameCard.Age);
+
+            //(Anonymous Type)무명 형식을 이용해서 완성 // don't need class 
+            var complex = new { Real = 3, Imaginary = -12 };
+            Console.WriteLine("Real : {0}, Imaginary : {1}", complex.Real, complex.Imaginary);
+            
+        }
+    }
+}
