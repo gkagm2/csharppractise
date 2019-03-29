151c7c7fd1d914655b2ca27fc4b30b09ce8bb9ca
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-30 05:59:28 +0900

    System.Array  <-

---
+++ practise1/practise/Array Collection Indexer/DerivedFromArray02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise.Array_Collection_Indexer
+{
+    class DerivedFromArray02
+    { 
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 10, 30, 20, 7, 1 };
+            Console.WriteLine("Type of array  : {0}", array.GetType());
+
+            Console.WriteLine("Base Type of array : {0}", array.GetType().BaseType);
+        }
+    }
+}
