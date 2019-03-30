fc176604b3b21e456043c14661dbc3992dfe3d41
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-31 03:47:16 +0900

    Hashtable    collections

---
+++ practise1/practise/Array Collection Indexer/UsingHashtable10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace practise1.practise.Array_Collection_Indexer
+{
+    class UsingHashtable10
+    {
+        // Hashtable ( key, value)
+        static void Main(string [] args)
+        {
+            Hashtable hash = new Hashtable();
+
+            hash["one"] = "first";
+            hash["two"] = "second";
+            hash["three"] = "3";
+            hash["four"] = "4";
+            hash["five"] = "5";
+
+            Console.WriteLine(hash["one"]);
+            Console.WriteLine(hash["two"]);
+            Console.WriteLine(hash["three"]);
+            Console.WriteLine(hash["four"]);
+            Console.WriteLine(hash["five"]);
+
+        }
+    }
+}
