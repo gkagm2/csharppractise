6fdf49451815b462bb7cbabec21b55c15bcccba1
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-31 03:46:56 +0900

    Stacl    Collections

---
+++ practise1/practise/Array Collection Indexer/UsingStack09.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections; // need
+using System.Text;
+
+namespace practise1.practise.Array_Collection_Indexer
+{
+    class UsingStack09
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while(stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
