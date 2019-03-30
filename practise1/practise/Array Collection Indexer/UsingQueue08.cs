c3c6e71dbc4b8a1b69d004c2f01677385416893a
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-31 03:46:45 +0900

    Queue    Collections

---
+++ practise1/practise/Array Collection Indexer/UsingQueue08.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections; // need
+using System.Text;
+
+namespace practise1.practise.Array_Collection_Indexer
+{
+    class UsingQueue08
+    {
+        static void Main(string [] args)
+        {
+            //queue
+            Queue queue = new Queue();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            while (queue.Count > 0){
+                Console.WriteLine(queue.Dequeue());
+            }
+            
+        }
+    }
+}
