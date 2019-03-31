a6d7d5b7605f1a041b73806600ae7be001f188b6
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-31 19:53:05 +0900

    Generic Stack

---
+++ practise1/practise/GenericProgramming/GenericStack06.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise.GenericProgramming
+{
+    class GenericStack06
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while(stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
