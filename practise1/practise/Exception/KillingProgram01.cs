bc88ef756678c8aff9fbe84fa2cd7646ffe38686
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-31 22:30:14 +0900

    occur error

---
+++ practise1/practise/Exception/KillingProgram01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise.Exception
+{
+    class KillingProgram01
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("exit");
+        }
+    }
+}
