652ea3ac59e88d767cd6caae7798af7871474eef
HyeonMyeongJang <gkagm2@gmail.com>
2019-03-31 19:52:43 +0900

    Generic Queue

---
+++ practise1/practise/GenericProgramming/GenericArrayList04.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise.GenericProgramming
+{
+    class GenericArrayList04
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for(int i=0;i<5; i++)
+            {
+                list.Add(i);
+            }
+            foreach(int element in list)
+            {
+                Console.Write("{0} ", element);
+            }
+            Console.WriteLine();
+            list.RemoveAt(2);
+
+            foreach(int element in list)
+            {
+                Console.Write("{0} ", element);
+            }
+            Console.WriteLine();
+
+            list.Insert(2, 12);
+
+            foreach(int element in list)
+            {
+                Console.Write("{0} ", element);
+            }
+            Console.WriteLine();
+
+
+            
+
+
+        }
+    }
+}
