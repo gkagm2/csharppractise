f338b4e9586e4b07eec780b1b2a544bfdb696ae1
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-01 21:33:03 +0900

    Exception TODO

---
+++ practise1/practise/Exception/MyException05.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise.Exception
+{
+    class MyException05 : System.Exception
+    {
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
