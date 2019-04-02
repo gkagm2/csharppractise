a9f6ded4be83ffc6ff0ea0b360c9cb97b6f5ae3b
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-02 20:32:35 +0900

    stack trace exception practise

---
+++ practise1/practise/Exception/StackTrace06.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise.Exception
+{
+    class StackTrace06
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 1;
+                Console.WriteLine(3 / --a);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+        }
+    }
+}
