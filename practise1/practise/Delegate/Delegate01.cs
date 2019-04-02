3f5bab4ecf07e6a0f0ed936dd8461afa9b283801
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-03 04:03:41 +0900

    delegate

--- practise1/practise/Delegate/Delegate01.cs
+++ practise1/practise/Delegate/Delegate01.cs
@@ -5,18 +5,33 @@ using System.Text;
 
 namespace practise1.practise.Delegate
 {
-    delegate int MyDelegate(int a, int b);
-    class Delegate01
+    //델리게이트의 선언
+    delegate int MyDelegate(int a, int b); 
+    class Calculator
     {
-        //델리게잍트 소스는 인스턴테 메소드도 참조할 수 이있구ㅗ
+        //delegate source는 인스턴스 메소드도 참조할 수 있고
         public int Plus(int a, int b)
         {
             return a + b;
         }
-        public static int Minus(int a, int b) {
+        //delegate는 정적 메소드도 참조할 수 있다.
+        public static int Minus(int a, int b)
+        {
             return a - b;
         }
-        //TODO
+    }
+    class Delegate01
+    {
+        static void Main(string[] args)
+        {
+            Calculator Calc = new Calculator();
+            MyDelegate Callback;
 
+            Callback = new MyDelegate(Calc.Plus);
+            Console.WriteLine(Callback(3, 4));
+
+            Callback = new MyDelegate(Calculator.Minus);
+            Console.WriteLine(Callback(7, 5));
+        }
     }
 }
