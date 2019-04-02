6de78ea22752f0c4d0b177425f43415e85d4c562
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-02 20:40:34 +0900

    TODO

---
+++ practise1/practise/Delegate/Delegate01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace practise1.practise.Delegate
+{
+    delegate int MyDelegate(int a, int b);
+    class Delegate01
+    {
+        //델리게잍트 소스는 인스턴테 메소드도 참조할 수 이있구ㅗ
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Minus(int a, int b) {
+            return a - b;
+        }
+        //TODO
+
+    }
+}
