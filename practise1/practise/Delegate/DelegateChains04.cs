f9c41b66c5c07ff8a8ab6fc871e733759329deed
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-04 03:58:58 +0900

    make Delegate chains

---
+++ practise1/practise/Delegate/DelegateChains04.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise.Delegate
+{
+    delegate void Notify(string message);
+    class Notifier
+    {
+        public Notify EventOccured;
+    }
+    class EventListener
+    {
+        private string name;
+        public EventListener(string name)
+        {
+            this.name = name;
+        }
+        public void SomethingHappend(string message)
+        {
+            Console.WriteLine("{0} .SomethingHappend : {1}", name, message);
+        }
+    }
+    class DelegateChains04
+    {
+        static void Main(string[] args)
+        {
+            Notifier notifier = new Notifier();
+            EventListener listener1 = new EventListener("Listener1");
+            EventListener listener2 = new EventListener("Listener2");
+            EventListener listener3 = new EventListener("Listener3");
+
+            // make chain with "+=" operator
+            notifier.EventOccured += listener1.SomethingHappend;
+            notifier.EventOccured += listener2.SomethingHappend;
+            notifier.EventOccured += listener3.SomethingHappend;
+
+            notifier.EventOccured("You've got mail.");
+
+            Console.WriteLine();
+
+            // removed chain
+            notifier.EventOccured -= listener2.SomethingHappend;
+            notifier.EventOccured("Download complete.");
+
+            Console.WriteLine();
+
+            //make chain with +,- operators
+            notifier.EventOccured = new Notify(listener2.SomethingHappend) + new Notify(listener3.SomethingHappend);
+
+            Console.WriteLine();
+
+            Notify notify1 = new Notify(listener1.SomethingHappend);
+            Notify notify2 = new Notify(listener2.SomethingHappend);
+
+            //make chain with Delegate.Commbine();
+            /*
+            notifier.EventOccured = (Notify)Delegate.Combine(notify1, notify2);
+            notifier.EventOccured("Fire!");
+            */
+            // you can make this code even use Delegate.Remove.
+            /*
+            notifier.EventOccured = (Notify)Delegate.Remove(notifier.EventOccured, notify2);
+            notifier.EventOccured("RPG!");
+            */
+        }
+    }
+}
