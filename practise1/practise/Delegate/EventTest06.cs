f1477116e24df48f8469b2b2420571fef31fdeec
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-04 04:03:19 +0900

    Event Handler

---
+++ practise1/practise/Delegate/EventTest06.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise.Delegate
+{
+    delegate void EventHandler(string message);
+
+    class MyNotifier
+    {
+        public event EventHandler SomethingHappend;
+        public void DoSomething(int number)
+        {
+            int temp = number % 10;
+            if( temp != 0 && temp % 3 == 0)
+            {
+                SomethingHappend(String.Format("{0} : 짝", number));
+            }
+        }
+    }
+
+    class EventTest06
+    {
+        static public void MyHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static public void Main(string[] args)
+        {
+            MyNotifier notifier = new MyNotifier();
+            notifier.SomethingHappend += new EventHandler(MyHandler);
+
+            for( int i=0;i<30;i++)
+            {
+                notifier.DoSomething(i);
+            }
+        }
+
+    }
+}
