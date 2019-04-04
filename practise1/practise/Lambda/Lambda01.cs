510aed76f681b53ebc55608d622a74bfc71fad6e
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-05 04:20:28 +0900

    Statement Lambda (문 람다)

---
+++ practise1/practise/Lambda/Lambda01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise.Lambda
+{
+
+    class Lambda01
+    {
+        //익명 메소드를 만들려면 델리게이트가 필요하다. C# 컴파일러는 형식 유추(Type Inference)라는 기능을 제공하여 생략해도 됨.
+        delegate int Calculate(int a, int b);
+
+        static void Main(string[] args)
+        {
+            //매개_변수_목록 => 식
+            Calculate calc = (a, b) => a + b; //두 개의 int 형식 매개 변수 a, b를 받아 이 둘을 더해 반환하는 익명 메소드를 람다식으로 만들었다.
+
+            Console.WriteLine("{0} + {1} : {2}", 3, 4, calc(3, 4));
+        }
+
+    }
+}
---
+++ practise1/practise/Lambda/StatementLambda01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise.Lambda
+{
+    //문 형식의 람다식
+    class StatementLambda01
+    {
+        delegate string Concatenate(string[] args);
+
+        static void Main(string[] args)
+        {
+            Concatenate concat = (arr) =>
+            {
+                string result = "";
+                foreach (string s in arr)
+                {
+                    result += s;
+                }
+                return result;
+            };
+            Console.WriteLine(concat(args));
+        }
+    }
+}
