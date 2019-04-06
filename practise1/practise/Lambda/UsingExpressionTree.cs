fe04585fa4d024e73db5098e6de76f174b2ab431
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-06 20:48:19 +0900

    Expression Tree

---
+++ practise1/practise/Lambda/UsingExpressionTree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise
+{
+    class UsingExpressionTree
+    {
+        static void Main(string [] args)
+        {
+
+        }
+    }
+}
