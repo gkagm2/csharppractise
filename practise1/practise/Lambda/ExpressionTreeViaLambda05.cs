db386f4b983be4a85548ee17431f52aeaf230c8b
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-07 11:21:59 +0900

    update

---
+++ practise1/practise/Lambda/ExpressionTreeViaLambda05.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace practise1.practise.Lambda
+{
+    class ExpressionTreeViaLambda05
+    {
+        static void Main()
+        {
+            //동적으로 식 트리를 만들기는 불가
+            Expression<Func<int, int, int>> expression = (a, b) => 1 * 2 + (a - b);
+            Func<int, int, int> func = expression.Compile();
+
+            //x = 7, y = 8
+            Console.WriteLine("1*2+({0}-{1}) = {2}", 7, 8, func(7, 8));
+
+        }
+    }
+}
