beba6588731fca37da1cfea7bec706868adf7163
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-12 07:32:58 +0900

    Linq from practise

---
+++ practise1/practise/Linq/From01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace practise1.practise.Linq
+{
+    class From01
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 2, 6, 4, 5, 3, 7, 8, 1, 10 };
+
+            var result = from n in numbers
+                         where n % 2 == 0
+                         orderby n
+                         select n;
+
+            foreach (int n in result)
+            {
+                Console.WriteLine("짝수 : {0}", n);
+            }
+        }
+    }
+}
