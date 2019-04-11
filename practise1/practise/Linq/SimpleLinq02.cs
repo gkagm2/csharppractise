77b88e75376b66fa609bedce95fc04c0b6fd42cf
HyeonMyeongJang <gkagm2@gmail.com>
2019-04-12 07:33:08 +0900

    Linq practise

---
+++ practise1/practise/Linq/SimpleLinq02.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace practise1.practise.Linq
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+
+    class SimpleLinq02
+    {
+        static void Main(string [] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile() { Name="Jang", Height=180},
+                new Profile() { Name="Hyeon", Height=172},
+                new Profile() { Name="Myeong", Height=181},
+                new Profile() { Name="Hong", Height=167},
+                new Profile() { Name="Fak", Height=178}
+            };
+
+            var profiles = from profile in arrProfile
+                           where profile.Height < 175
+                           orderby profile.Height
+                           select new
+                           {
+                               Name = profile.Name,
+                               InchHeight = profile.Height * 0.393
+                           };
+            foreach(var profile in profiles)
+            {
+                Console.WriteLine("{0}, {1}", profile.Name, profile.InchHeight);
+            }
+        }
+    }
+}
