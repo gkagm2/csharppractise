10316ec2e863a9e5ede8aaca94af1483e43e8993
HyeonMyeongJang <gkagm2@gmail.com>
2019-05-04 15:29:46 +0900

    Stack

---
+++ practise1/practise/Array Collection Indexer/UsingStack09_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+            stack.Push(7);
+
+
+            foreach(int number in stack)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(stack.Count); // Stack에 총 items의 수
+            Console.WriteLine(stack.Contains(3)); //3이 포함되어있는지 ?  true or false 반환
+            Console.WriteLine(stack.Peek()); // 맨 위에 개체를 제거하지 않고 반환한다.
+        }
+    }
+}
