34dd0e662fc1b9ec78ddfc9cd37ad615ffd7c88f
HyeonMyeongJang <gkagm2@gmail.com>
2019-08-16 23:23:35 +0900

    static value of class test

---
+++ practise1/practise/StaticPractise.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise1.practise
+{
+    public class TestClass{
+        static int count = 0;
+        public TestClass()
+        {
+            count++;
+            Console.WriteLine("count : " + count);
+        }
+    }
+    class StaticPractise
+    {
+        static void Main(string[] argv)
+        {
+            TestClass testClass = new TestClass();
+            TestClass testClass2 = new TestClass();
+        }
+        
+        
+    }
+}
